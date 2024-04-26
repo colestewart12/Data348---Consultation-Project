c1ad469c32cf354d39d77706b29d86e1c9229aab
Allen Tugade <165172825+allen-tugade@users.noreply.github.com>
2024-04-26 13:13:40 -0700

    Update cleaner.py
    added encoding

--- Data/cleaner.py
+++ Data/cleaner.py
@@ -2,12 +2,80 @@ import os
 import pandas as pd
 
 # read CSV file
-df = pd.read_csv('Student Financial Data Project.csv')
+SFA = pd.read_csv('SFA_Cleaned.csv')
 
 # drop all rows except first entry for each unique UID
-df.drop_duplicates(subset='UID', keep='first', inplace=True)
-# drop all students who have no fafse
-df = df[df['EFC'] != "No FAFSA"]
+SFA.drop_duplicates(subset='UID', keep='first', inplace=True)
+# drop all students who have no fafsa
+SFA = df[df['EFC'] != "No FAFSA"]
+
+data = []
+
+# 1/0 encode each race group and dichotomous variables
+
+for row in SFA:
+    row_data = []
+    
+    if row[12] == 'Male':
+        row_data.append(1.0)
+    else:
+        row_data.append(0.0)
+    
+    if row[12] == 'Female' or row[12] == 'Unknown':
+        row_data.append(1.0)
+    else:
+        row_data.append(0.0)
+    
+    if row[13] == 'American Indian or Native Alaskan':
+        row_data.append(1.0)
+    else:
+        row_data.append(0.0) 
+
+    if row[13] == 'Asian':
+        row_data.append(1.0)
+    else:
+        row_data.append(0.0) 
+   
+    if row[13] == 'Black or African American':
+        row_data.append(1.0)
+    else:
+        row_data.append(0.0)
+
+    if row[13] == 'Hispanic':
+        row_data.append(1.0)
+    else:
+        row_data.append(0.0) 
+
+    if row[13] == 'Native Hawaiian or Pacific Islander':
+        row_data.append(1.0)
+    else:
+        row_data.append(0.0) 
+
+    if row[13] == 'White':
+        row_data.append(1.0)
+    else:
+        row_data.append(0.0) 
+
+    if row[16] == 'Yes':
+        row_data.append(1.0)
+    else:
+        row_data.append(0.0)
+
+    if row[17] == 'Yes':
+        row_data.append(1.0)
+    else:
+        row_data.append(0.0)
+
+    row_data.append(float(row[25])/float(row[2]))
+   
+    row_data.append(float(row[26])/float(row[2]))
+   
+    if row[23] == 'NA':
+        row_data.append(0.0)
+    else:
+        row_data.append(float(row[23])/float(row[2]))
+   
+    data.append(row_data)
 
 variables = ['CURR_GPA', 'FIRST_YEAR_CREDITS', 'DFWIS', 'PRIOR_SEMESTERS_COMPLETED',
              'IN_STATE', 'INTERNATIONAL', 'GENDER', 'ETHNICITY', 'FIRST_YEAR_ON_CAMPUS',
