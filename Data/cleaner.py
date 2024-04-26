5b5625df1858dbd56f1b1459054fcba4886c9ced
Camden Hunt <114128598+isCamden@users.noreply.github.com>
2024-04-26 12:51:40 -0700

    added data cleaner script

---
+++ Data/cleaner.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+
+# read CSV file
+df = pd.read_csv('Student Financial Data Project.csv')
+
+# drop all rows except first entry for each unique UID
+df.drop_duplicates(subset='UID', keep='first', inplace=True)
+
+variables = ['CURR_GPA', 'FIRST_YEAR_CREDITS', 'DFWIS', 'PRIOR_SEMESTERS_COMPLETED',
+             'IN_STATE', 'INTERNATIONAL', 'GENDER', 'ETHNICITY', 'FIRST_YEAR_ON_CAMPUS',
+             'AGE', 'FGEN', 'FIRST_YEAR_MEAL_PLAN', 'WORK_STUDY', 'PELL', 'LOAN',
+             'NON_PLU_SCHOLARSHIP', 'PLU_SCHOLARSHIP', 'EFC']
+
+# make folder
+folder_name = "split_csv"
+os.makedirs(folder_name, exist_ok=True)
+
+for var in variables:
+    # make individual csvs for each variable
+    data = df[['UID', 'TERM', var, 'GRADUATED']].copy()
+    # remove rows with null values
+    data.dropna(inplace=True)
+    # save
+    data.to_csv(f"{folder_name}/{var}.csv", index=False)
